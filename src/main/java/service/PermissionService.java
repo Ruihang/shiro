67131e68cd9779aa6105ce169c91a4533b334c0f
huangruixian <ruihang@foxmail.com>
2018-07-18 14:56:46 +0800

    finished chapter7

--- src/main/java/service/PermissionService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import entity.Permission;
-
-public interface PermissionService {
-    Permission createPermission(Permission permission);
-
-    void deletePermission(Long permissionId);
-}  
